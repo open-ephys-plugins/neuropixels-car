c44bf95e829df4a2763ca9a1ecf1d0669df42da7
Anjal Doshi <anjaldoshi09@gmail.com>
2025-05-12 19:03:45 -0700

    Bump plugin version to 1.0.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -43,7 +43,7 @@ extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
     The GUI refueses to load plugins with mismatched API versions */
     info->apiVersion = PLUGIN_API_VER;
     info->name = "Neuropixels CAR"; // <---- update
-    info->libVersion = "0.1.0"; // <---- update
+    info->libVersion = "1.0.0"; // <---- update
     info->numPlugins = NUM_PLUGINS;
 }
 
